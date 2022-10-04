3bd81c466cf0810e5697605bec903ba8ee4409af
wanjiku <annritawanjiku77@gmail.com>
2022-10-04 23:03:34 +0300

    completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const theDirectors = directors.map((director)=>(
+    <div key={director.name}>
+
+      <h2>{director.name}</h2>
+        <ul>
+          {director.movies.map((movie) => (
+            <li key={movie}>{movie}</li>
+          ))}
+        </ul>
+    </div>
+  ))
+  return <div>
+    {/*{code here}*/}
+    <h1>Directors Page</h1>
+    <React.Fragment>{theDirectors}</React.Fragment>
+    </div>;
 }
 
 export default Directors;
